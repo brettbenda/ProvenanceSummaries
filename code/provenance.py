122c54bc078dce9d43b22d3c91eb9db014713c42
Brett Benda <brett.benda@ufl.edu>
2021-12-14 12:21:39 -0500

    comments

--- code/provenance.py
+++ code/provenance.py
@@ -2,15 +2,16 @@ import json
 import csv
 import sys
 
-
+##How to access a doc from docs var
 ##docs[set#][#]
 docs = []
 
+##How to access a log or segment item from logs/segments var
 ##item[set#][participant#][#]
 logs = []
 segments = []
 
-path1 = "ProvSegments/Dataset_" #dataset num
+path1 = "ProvSegments/Dataset_" 
 path2 = "/User Interactions/"
 path21 = "/Segmentation/"
 path3 = "_P" #user num
@@ -18,17 +19,9 @@ path4 = "_InteractionsLogs.json"
 path41 = "_20_4_6_Prov_Segments.csv"
 
 mergesegments = int(sys.argv[1])
-print(mergesegments)
 
-def setToString(i):
-    if (i==1):
-        return "Arms"
-    elif (i==2):
-        return "Terrorist"
-    else:
-        return "Disappearance"
-
-
+#Set names
+setNames = ["Arms", "Terrorist", "Disappearance"]
 
 #Open doc JSON
 with open('ProvSegments/Dataset_1/Documents/Documents_Dataset_1.json', encoding="utf8") as file:
@@ -39,15 +32,18 @@ with open('ProvSegments/Dataset_3/Documents/Documents_Dataset_3.json', encoding=
     docs.append(json.load(file))
 
 #Open log/segment JSON
-for i in range (1,4):
+for i in range (1,4): ##3 datasets
     setlogs = []
     setsegments = []
-    for j in range (1,9):
-        with open(path1+str(i)+path2+setToString(i)+path3+str(j)+path4) as file:
+    for j in range (1,9): ##8 participants
+
+        ##Getting log json for dataset/participant
+        with open(path1+str(i)+path2+setNames[i-1]+path3+str(j)+path4) as file:
             file_json = json.load(file)
             setlogs.append(file_json)
 
-        with open(path1+str(i)+path21+setToString(i)+path3+str(j)+path41) as file:
+        ##Getting segments and converting CSV to json for dataset/participant
+        with open(path1+str(i)+path21+setNames[i-1]+path3+str(j)+path41) as file:
             reader = csv.DictReader(file)
             csvjson = [json.dumps(d) for d in reader]
             setsegments.append(csvjson);
@@ -57,22 +53,31 @@ for i in range (1,4):
 
 #Create segment JSON, cluster short segments into previous segments
 segment_json = []
-min_segment_length = 180
+min_segment_length = 0;
+
+#for each dataset and participant
 for _set in range(0,3):
     for _id in range(0,8):
+
+        ##default state for new set of segments
         current_segment = 0
         new_segment = True;
         segment_start = 0;
         segment_end = 0;
         current_segment_json = []
+
+        #for all the corresponding segments
         for i,segment in enumerate(segments[_set][_id]):
+
+            ##string-indexable segment
             stringjson = json.loads(segment)
 
+            ##get min size, ignore if arg was 0;
             min_segment_length = float(json.loads(segments[_set][_id][-1])['end'])/24
             if mergesegments == 0:
                 min_segment_length = 0
 
-
+            ##get start and end times
             segment_start = int(float(stringjson['start']))
             segment_end = int(float(stringjson['end']))
 
@@ -103,11 +108,14 @@ for _set in range(0,3):
                 current_segment_json.append(item)
 
                 current_segment = current_segment+1
+        ##calculate length for resulting sections        
         for segment in current_segment_json:
             segment.update({"length" : int(segment['end']-segment['start'])})
             segment_json.append(segment)
 
-#count number of items that WOULD be in the segment.
+#After the previous set of loops, we have JSON for segments.
+
+#Now we go through and add pid, segment ids, dataset ids, and counts for how many interactions are in the segment
 _segment = 1;
 for _set in range(0,3):
     for _id in range(0,8):
